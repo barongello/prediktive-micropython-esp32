9d69c747d1fec53e8e799d3b2941b0ea86993bba
barongello <wagner@barongello.com.br>
2026-07-27 01:27:22 -0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -4,6 +4,30 @@ from camera import Camera, FrameSize, PixelFormat
 import st7789
 import vga1_8x16
 import vga2_8x16
+import prediktive_driver
+import prediktive_library
+
+button_last_press = 0
+led_state = False
+
+def button_handler(_):
+    global button_last_press
+    global led_state
+
+    now = time.ticks_ms()
+
+    if time.ticks_diff(now, button_last_press) < 200:
+        return
+
+    button_last_press = now
+
+    led_state = not led_state
+
+    prediktive_driver.set_led(led_state)
+
+prediktive_driver.on_button_press(button_handler)
+
+prediktive_driver.set_led(False)
 
 print('Initializing ST7789 display...')
 
@@ -17,10 +41,10 @@ spi = SPI(
 )
 
 display = st7789.ST7789(
-    spi, 
+    spi,
     240, 320,
-    reset=Pin(2, Pin.OUT), 
-    dc=Pin(3, Pin.OUT), 
+    reset=Pin(2, Pin.OUT),
+    dc=Pin(3, Pin.OUT),
     cs=Pin(15, Pin.OUT),
     rotation=1
 )
@@ -34,7 +58,7 @@ print('Initializing camera...')
 try:
     cam = Camera(pixel_format=PixelFormat.RGB565, frame_size=FrameSize.QVGA)
 
-    time.sleep(2.0)                      
+    time.sleep(2.0)
 
     print('Camera initialized!')
 except Exception as e:
@@ -48,18 +72,18 @@ if cam:
     fps = 0
     fps_frames = 0
     fps_timer = 0
-    fps_last_frame_time = time.ticks_us()
+    fps_last_frame_time_ms = time.ticks_ms()
 
     while True:
-        fps_now = time.ticks_us()
-        fps_dt = fps_now - fps_last_frame_time
-        fps_last_frame_time = fps_now
+        fps_now_ms = time.ticks_ms()
+        fps_dt_ms = fps_now_ms - fps_last_frame_time_ms
+        fps_last_frame_time_ms = fps_now_ms
 
-        fps_frames += 1
-        fps_timer += fps_dt
+        fps_frames = prediktive_library.add(fps_frames, 1)
+        fps_timer += fps_dt_ms / 1000
         
-        if fps_timer >= 1000:
-            fps = fps_frames * 1000000 / fps_timer
+        if fps_timer >= 1:
+            fps = fps_frames / fps_timer
 
             fps_frames = 0
             fps_timer = 0
@@ -79,7 +103,7 @@ if cam:
                 print('capture: {} ms | blit: {} ms | frame time: {} ms | fps: {}'.format(
                     time.ticks_diff(t1, t0) / 1000,
                     time.ticks_diff(t2, t1) / 1000,
-                    fps_dt / 1000,
+                    fps_dt_ms,
                     fps
                 ))
             else:
@@ -93,5 +117,5 @@ if cam:
 
             time.sleep(0.5)
 
-        display.text(vga1_8x16, 'FPS: ', 10, 10, st7789.BLACK, st7789.WHITE)
-        display.text(vga2_8x16, '{}'.format(fps), 50, 10, st7789.BLACK, st7789.WHITE)
+        display.text(vga1_8x16, 'FPS: ', 10, 10, st7789.WHITE, st7789.BLACK)
+        display.text(vga2_8x16, '{}'.format(fps), 50, 10, st7789.WHITE, st7789.BLACK)
